cb49a2421af57a4b4b3540482666b33ce4f34c80
Zachary Beyer <zack@boomlearning.com>
2023-11-12 13:44:22 -0700

    Not sure why these files were generated.

--- client/components/debug/print.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var templating_1 = require("meteor/templating");
-templating_1.Template.printJSON.helpers({
-    printJSON: function () {
-        return JSON.stringify(templating_1.Template.instance().data, null, "\t");
-    }
-});
